fe9d15ebc62963ec48c8d7ed7da6638a03e2bda7
1179542335 <1179542335@qq.com>
2015-10-26 23:49:59 +0800

    限制密码长度不少于6

--- user_ok.php
+++ user_ok.php
@@ -10,11 +10,16 @@ $info=mysql_fetch_array($sql);
 if($info==true){
 	echo "<script language='javascript'>alert('用户已被注册!');history.back();</script>";
 }else{
-	$sql=mysql_query("insert into tb_user (name,password,email,phone,admin) values('$name','$pwd','$email','$phone','0')");
-	if($sql==true){
-		echo "<script language='javascript'>alert('用户添加成功!');window.location='index.php';</script>";
+
+	if(strlen($pwd)>=6){
+		$sql=mysql_query("insert into tb_user (name,password,email,phone,admin) values('$name','$pwd','$email','$phone','0')");
+		if($sql==true){
+			echo "<script language='javascript'>alert('用户添加成功!');window.location='index.php';</script>";
+		}else{
+			echo "<script language='javascript'>alert('用户添加失败!');history.back();</script>";
+		}
 	}else{
-		echo "<script language='javascript'>alert('用户添加失败!');history.back();</script>";
+	echo "<script language='javascript'>alert('密码应大于6位!');history.back();</script>";
 	}
 }
 //strlen()
